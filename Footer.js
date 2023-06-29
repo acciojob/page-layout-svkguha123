00413e7d970522d11621ef719e88d05ffe347d51
Souvik Guha <117675254+svkguha123@users.noreply.github.com>
2023-06-29 13:50:05 +0530

    complete

---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+let Footer = ()=> {
+    return(
+        <div>
+            <h3>2023 my website. All rights reserved.</h3>
+            
+        </div>    
+    )
+}
+
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+let Header = (props)=> {
+    return(
+        <div>
+            <h1>Welcome to my website.</h1>
+            {props.children}
+        </div>    
+    )
+}
+
+export default Header;
